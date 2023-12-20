3d44e5f5ed998b427995c0bb144c5626f6216a4c
Nelson <nallauca@tutanota.com>
2023-12-20 09:13:15 +0100

    - Solve log in problem - Include attached file in the message

--- simple-mailing-client/main.py
+++ simple-mailing-client/main.py
@@ -1,28 +1,60 @@
+##################
+# For this mailling client to work, if the mail account is a google account, first make 
+# sure that the 2 step verification is enable (https://support.google.com/accounts/answer/185839?sjid=15619175640662600021-EU)
+# and the app password is generated (https://support.google.com/accounts/answer/185833?sjid=15619175640662600021-EU#zippy=%2Cwhy-you-may-need-an-app-password%2Capp-passwords-revoked-after-password-change)
+##################
 import smtplib
 from email import encoders
 from email.mime.text import MIMEText #Ordinary test to use
 from email.mime.base import MIMEBase #For attachment
 from email.mime.multipart import MIMEMultipart #Fro the hole thing
 
-server = smtplib.SMTP('smtp.gmail.com',465) #Specify the smtp server hostname and port
-
-server.ehlo
+# Connect to the SMTP server using TLS
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.ehlo()
 
 with open('pass.txt', 'r') as file: #open pass.tx as "file" alias
     password = file.read()
 
-#log in to the mail server
-server.login('datanetec@gmail.com', password)
+try:
+    # Log in to the mail server
+    server.login('nelson.allauca@ecreationmedia.tv', password)
+    print("Login successful!")
+except smtplib.SMTPAuthenticationError as e:
+    print(f"Login failed. SMTP Authentication Error: {e}")
 
 #Define message (Header)
 msg = MIMEMultipart()
-msg['From'] = 'Test Sender'
-msg['To'] = 'nallauca@tutanota.com'
+msg['From'] = 'Nelson Allauca'
+msg['To'] = 'nallauca@protonmail.com'
 msg['Subject'] = 'This is a test email about python.'
 
 #Load the message
 with open('attachment.txt', 'r') as file:
-    message = file.read
+    message = file.read()
 
-#something to attach to the message
-msg.attach(MIMEText(message,'plain')) #attach file as plain text
+print(message)
+
+# Attach something to the message
+msg.attach(MIMEText(message, 'plain'))
+
+# Attach an image file
+img_file = 'coding.jpeg' #sometimes the whole path is needed here
+
+img_attachment = open(img_file, 'rb') #rb = read byte codes
+
+#Create payload object
+payload = MIMEBase ('application','octet-stream')
+payload.set_payload (img_attachment.read())
+
+#use encoders
+encoders.encode_base64(payload)
+payload.add_header ('Content-Disposition', f'attachment; filename={img_file}')
+msg.attach(payload)
+
+text = msg.as_string() #Finaly got everything as string
+server.sendmail('Nelson Allauca','nallauca@protonmail.com',text)
+
+# Close the connection
+server.quit()
